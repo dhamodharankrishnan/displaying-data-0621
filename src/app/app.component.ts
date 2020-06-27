0a204be0d4f076299a60cd1cbc04b290268b220e
Dhamodharan Krishnan <dhamodharan.krishnan@walgreens.com>
2020-06-27 12:37:41 -0400

    Used a class to represent data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,32 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
   selector: 'app-root',
   template: `
     <h1>{{title}}</h1>
-    <h2>My favorite hero is: {{myHero}}</h2>
+    <h2>My favorite hero is: {{myHero.name}}</h2>
     <p>Heroes:</p>
     <ul>
       <li *ngFor="let hero of heroes">
-        {{ hero }}
+        Name : {{ hero.name }}
+         Phone : {{ hero.phoneNumber }}
       </li>
     </ul>
     `
 })
 export class AppComponent {
   title: string;
-  myHero: string;
-  heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
+  myHero: Hero;
+  heroes = [
+    new Hero(1, 'Windstorm', 2247039966),
+    new Hero(13, 'Bombasto', 1234567890),
+    new Hero(15, 'Magneta', 939783498),
+    new Hero(20, 'Tornado', 298472394)
+  ];
 
   constructor() {
     this.title = 'Tour of Heroes';
-    this.myHero = 'Windstorm';
+    this.myHero = this.heroes[0];
   }
 }
